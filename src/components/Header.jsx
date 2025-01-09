80835925e0ea2be30e015b15aebfd31d560f9b94
Sanju Devadas <sanjudevadas11@gmail.com>
2025-01-09 11:44:03 +0530

    added css for login page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 import Logo from '../assets/Logonetflix.png'
 const Header = () => {
   return (
-    <div className='absolute px-8 py-2 bg-gradient-to-b from-black'>
+    <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10'>
         <img className='w-44' src={Logo} alt="Logo" />
         </div>
   )
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,47 @@
-//import React from 'react'
+import Header from "./Header";
+import netflixbg from "../assets/netflixbg.jpg";
 
-import Header from "./Header"
-import netflixbg from '../assets/netflixbg.jpg'
 const Login = () => {
   return (
-    <div>
-        <Header/>
-   <div>
-<img src={netflixbg} alt="background" />
-   </div>
-   
-   </div>
+    <div className="relative min-h-screen bg-black">
+      <Header />
+      <div className="absolute inset-0">
+        <img
+          src={netflixbg}
+          alt="background"
+          className="w-full h-full object-cover brightness-50"
+        />
+      </div>
 
+      <form
+        action=""
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-11/12 max-w-md p-8 bg-black/75 rounded-lg shadow-lg"
+      >
+        <h1 className="text-3xl font-bold text-white mb-6 text-center">
+          Sign In
+        </h1>
+        <input
+          type="text"
+          placeholder="Email"
+          className="w-full p-3 mb-4 bg-gray-800 text-white rounded focus:outline-none focus:ring-2 focus:ring-red-600"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full p-3 mb-6 bg-gray-800 text-white rounded focus:outline-none focus:ring-2 focus:ring-red-600"
+        />
+        <button className="w-full py-3 bg-red-600 hover:bg-red-700 text-white font-semibold rounded transition duration-300">
+          Sign In
+        </button>
+        <p className="text-gray-400 text-center mt-4">
+          New to Netflix?{" "}
+          <a href="/signup" className="text-white hover:underline">
+            Sign up now
+          </a>
+        </p>
+      </form>
+    </div>
+  );
+};
 
-        
-  )
-}
-
-export default Login
+export default Login;
