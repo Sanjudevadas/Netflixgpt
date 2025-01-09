984021462674c12e41f7f75e8baebab7b69c5c4a
Sanju Devadas <sanjudevadas11@gmail.com>
2025-01-09 11:17:55 +0530

    created header and added logo with gradient

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@
 import Logo from '../assets/Logonetflix.png'
 const Header = () => {
   return (
-    <div>
-        <img src={Logo} alt="Logo" />
+    <div className='absolute px-8 py-2 bg-gradient-to-b from-black'>
+        <img className='w-44' src={Logo} alt="Logo" />
         </div>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,19 @@
 //import React from 'react'
 
 import Header from "./Header"
-
+import netflixbg from '../assets/netflixbg.jpg'
 const Login = () => {
   return (
     <div>
         <Header/>
-        </div>
+   <div>
+<img src={netflixbg} alt="background" />
+   </div>
+   
+   </div>
+
+
+        
   )
 }
 
