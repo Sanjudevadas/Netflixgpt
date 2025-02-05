82a533951f7565d23cd5687ba1aadd56e7107b3e
Sanju Devadas <sanjudevadas11@gmail.com>
2025-02-05 11:27:31 +0530

    added apis in browse page

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Yjc3YjdiNzI5M2YzYmQ2MjdjOGZjMzdlOGYzY2M3YiIsIm5iZiI6MTcxODM4NDYzNy4xMTYsInN1YiI6IjY2NmM3N2ZkOWFjOTI0NDBjOWY4MWRjMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nLGMTUTP6FlyyHBVt02Q370ffeI66pqAhySsB-RxCms'
+    }
+  };
