e781470ab11475978f4a9d44ac3884e2b1275c74
Sanju Devadas <sanjudevadas11@gmail.com>
2025-02-06 11:12:43 +0530

    editing the ui

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import VideoTitle from './VideoTitle'
+import MovieBackground from './MovieBackground'
 
 const MainContainer = () => {
 
     const movies = useSelector(store=> store.movies?.nowPlayingMovies)
   return (
-    <div>MainContainer</div>
+    <div>
+      <VideoTitle/>
+      <MovieBackground/>
+      
+      </div>
   )
 }
 
