5ad22a6b552d0a939096f0b2f46b280ed30b0c44
Sanju Devadas <sanjudevadas11@gmail.com>
2025-02-07 20:25:38 +0530

    adding video bg

--- src/components/MovieBackground.jsx
+++ src/components/MovieBackground.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { API_OPTIONS } from "../utils/constants";
 
 const MovieBackground = ({ movieId }) => {
+
+  const getMovieVideos = async ()=>{
+
+    const data = await fetch(`https://api.themoviedb.org/3/movie/{movie_id}/videos`,API_OPTIONS)
+  }
   return (
     <div className="absolute top-0 left-0 w-full h-[80vh] -z-10">
      
