8c3b59e4b0dac4937e44307ee244b0c93d658c80
Sanju Devadas <sanjudevadas11@gmail.com>
2025-03-06 12:44:13 +0530

    Gpt Search

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,20 +1,27 @@
-
 import Header from "./Header";
 
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies.js";
 import MainContainer from "./MainContainer.jsx";
 import SecondaryContainer from "./SecondaryContainer.jsx";
 import usePopularMovies from "../hooks/usePopularMovies.js";
-
+import GptSearch from "./GptSearch.jsx";
+import { useSelector } from "react-redux";
 
 const Browse = () => {
-   useNowPlayingMovies();
-   usePopularMovies();
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
+  useNowPlayingMovies();
+  usePopularMovies();
   return (
     <div>
       <Header />
-      <MainContainer/>
-      <SecondaryContainer/>
+      {showGptSearch ? (
+        <GptSearch />
+      ) : (
+        <>
+          <MainContainer />
+          <SecondaryContainer />
+        </>
+      )}
     </div>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,56 +1,56 @@
-import { onAuthStateChanged, signOut } from 'firebase/auth';
-import Logo from '../assets/Logonetflix.png';
-import usericon from '../assets/netflix_usericon.jpg';
-import { auth } from '../utils/firebase';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { addUser, removeUser } from '../utils/userSlice';
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import Logo from "../assets/Logonetflix.png";
+import usericon from "../assets/netflix_usericon.jpg";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { addUser, removeUser } from "../utils/userSlice";
+import { toggleGptSearchView } from "../utils/gptSlice";
 
 const Header = () => {
-
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   const navigate = useNavigate();
-  const user =useSelector((store)=> store.user)
+  const user = useSelector((store) => store.user);
 
-const handleSignOut= ()=>{
-  signOut(auth).then(()=>{
-    // Sign Out Successful
-    navigate("/")
-  }).catch((error)=>{
-// Error happened
-navigate("/error")
-  })
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign Out Successful
+        navigate("/");
+      })
+      .catch((error) => {
+        // Error happened
+        navigate("/error");
+      });
+  };
 
-  
-}
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const { uid, email, displayName } = user;
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
+        navigate("/browse");
+      } else {
+        // User is signed out
+        // ...
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
 
- useEffect(()=> {
+    //Unsubscribe when component unmounts
+    return () => unsubscribe();
+  }, []);
 
-      const unsubscribe =    onAuthStateChanged(auth, (user) => {
-            if (user) {
-              // User is signed in, see docs for a list of available properties
-              // https://firebase.google.com/docs/reference/js/auth.user
-              const {uid,email,displayName} = user;
-              dispatch(addUser({uid:uid ,email:email,displayName:displayName})
-            
-            
-            );
-              navigate("/browse");
-              
-            } else {
-              // User is signed out
-              // ...
-             dispatch(removeUser())
-             navigate("/");
+  const handleGptSearchClick = ()=>{
+//toggle gptsearch
+dispatch(toggleGptSearchView());
 
-            }
-          });
-
- //Unsubscribe when component unmounts
- return ()=> unsubscribe();
-        },[])
+  }
 
   return (
     <header className="absolute top-0 w-full px-8 py-3 bg-gradient-to-b from-black z-20 flex items-center justify-between">
@@ -58,13 +58,26 @@ navigate("/error")
       <img className="w-40 md:w-44" src={Logo} alt="Netflix Logo" />
 
       {/* User Section */}
-      { user && (<div className="flex items-center gap-4">
-        <img className="w-10 h-10 md:w-12 md:h-12   border-gray-300" src={usericon} alt="User Icon" />
-        
-        <button onClick={handleSignOut} className="text-white px-4 py-2 text-sm md:text-base font-semibold rounded-md border border-gray-500 hover:bg-gray-700 transition">
-          Sign Out
-        </button>
-      </div> )}
+      {user && (
+        <div className="flex items-center gap-4">
+          <button className="py-2 px-4 m-2  bg-purple-800 text-white rounded-lg"
+           onClick={handleGptSearchClick}         >
+            GPT Search
+          </button>
+          <img
+            className="w-10 h-10 md:w-12 md:h-12   border-gray-300"
+            src={usericon}
+            alt="User Icon"
+          />
+
+          <button
+            onClick={handleSignOut}
+            className="text-white px-4 py-2 text-sm md:text-base font-semibold rounded-md border border-gray-500 hover:bg-gray-700 transition"
+          >
+            Sign Out
+          </button>
+        </div>
+      )}
     </header>
   );
 };
