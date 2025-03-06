fadc68461fb3e12edbaead58cef1b2a21631afbc
Sanju Devadas <sanjudevadas11@gmail.com>
2025-03-06 14:23:18 +0530

    added search bar

--- src/components/GptSearch.jsx
+++ src/components/GptSearch.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import GptSearchBar from './GptSearchBar'
 import GptMovieSuggetions from './GptMovieSuggetions'
-
+import netflixbg from "../assets/netflixbg.jpg"
 const GptSearch = () => {
   return (
     <div>
+
+        <div className='absolute -z-10'>
+            <img src={netflixbg} alt="" />
+        </div>
         <GptSearchBar/>
         <GptMovieSuggetions/>
         </div>
